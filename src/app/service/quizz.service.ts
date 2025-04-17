f90cb0af09b27207924ad774566cb4af53c8107a
Youssef Ait Rais <copybuisness007@gmail.com>
2025-04-17 11:24:08 +0200

    services api

--- src/app/service/quizz.service.ts
+++ src/app/service/quizz.service.ts
@@ -1,9 +1,23 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+
+export interface quizzQuestion {
+  category : String;
+  type: String ;
+  difficulty : String;
+  question : String;
+  correct_answer : String;
+  incorrect_answer : String[];
+  all_answer: String[];
+}
 
 @Injectable({
   providedIn: 'root'
 })
 export class QuizzService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+  
 }
